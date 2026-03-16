5ae8b85827bb09afbe3335f63b4216604baaf356
alexneyro <alexneyro99@gmail.com>
2026-03-16 18:20:35 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// Если у тебя в проекте используется Tailwind 4, раскомментируй строку ниже и добавь tailwindcss() в plugins
-// import tailwindcss from '@tailwindcss/vite';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   // Указываем базовый путь для GitHub Pages
   base: './', 
   plugins: [
     react()
-    // tailwindcss() // Раскомментируй, если билд потребует Tailwind
+    tailwindcss()
   ],
   resolve: {
     alias: {
