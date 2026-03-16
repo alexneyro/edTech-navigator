4df678a4ad518986ba88870eca8b6a3ff34aa9dc
alexneyro <alexneyro99@gmail.com>
2026-03-16 18:21:53 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
-  // Указываем базовый путь для GitHub Pages
-  base: './', 
+  // Заменяем на название твоего репозитория
+  base: '/edTech-navigator/', 
   plugins: [
-    react()
+    react(),
     tailwindcss()
   ],
   resolve: {
     alias: {
-      // Настройка @ для путей (чтобы не падали импорты)
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'dist',
-  }
 });
